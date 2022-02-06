34e1ab7386d349db62055529e1d3c9c3268248d7
TechCentreUK <support@techcentreuk.co.uk>
2022-02-06 14:41:25 +0000

    Add Event Models

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Month, Event
 
 # Register your models here.
+
+
+class MonthAdmin(admin.ModelAdmin):
+    """ Admin Month Display Fields """
+    list_display = ('name', 'friendly_name')
+
+class EventAdmin(admin.ModelAdmin):
+    """ Admin Event Display Fields """
+    list_display = ('name', 'date')
+
+admin.site.register(Month, MonthAdmin)
+admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Month(models.Model):
+    """ A Model for months category """
+
+    class Meta:
+        """ Verbose name for admin """
+        verbose_name_plural = 'Month'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def get_friendly_name(self):
+        """ Get friendly / front end name """
+        return f"{self.friendly_name}"
+
+
+class Event(models.Model):
+    """ A Model for events """
+    month = models.ForeignKey('Month', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    date = models.CharField(max_length=254)
+    guests = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date}"

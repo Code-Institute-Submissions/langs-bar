f1f9645bd081d819ec4b904a80fbc63febf01a67
TechCentreUK <support@techcentreuk.co.uk>
2022-02-08 19:16:13 +0000

    Update event details page to display an event

--- events/views.py
+++ events/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Event
 
 # Create your views here.
 
 
-def events(request):
+def all_events(request):
     """ A View to return the events.html """
-    return render(request, 'events/events.html')
+    events = Event.objects.all()
+    context = {
+        'events': events
+    }
+    return render(request, 'events/events.html', context)
+
+def event_detail(request, event_id):
+    """ A View to return an individual event details """
+    event = get_object_or_404(Event, pk=event_id)
+    context = {
+        'event': event
+    }
+    return render(request, 'events/event_detail.html', context)

34a8370561967fddff784f648f1a1faf9be72518
TechCentreUK <support@techcentreuk.co.uk>
2022-02-10 20:46:31 +0000

    Update event details and Add JS for quantity

--- langs/settings.py
+++ langs/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'location',
     'dresscode',
     'contact',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- langs/urls.py
+++ langs/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('location/', include('location.urls')),
     path('dresscode/', include('dresscode.urls')),
     path('contact/', include('contact.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

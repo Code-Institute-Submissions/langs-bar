fa32a0c43252e3cf7a1e22fab92d312a9266f148
TechCentreUK <support@techcentreuk.co.uk>
2022-02-11 01:10:02 +0000

    Update bag app & Add to bag function

---
+++ bag/contexts.py
@@ -0,0 +1,38 @@
+from django.shortcuts import get_object_or_404
+from events.models import Event
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    event_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            event = get_object_or_404(Event, pk=item_id)
+            total += item_data * event.price
+            event_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'event': event,
+            })
+        else:
+            event = get_object_or_404(Event, pk=item_id)
+            for quantity in item_data.items():
+                total += quantity * event.price
+                event_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'event': event,
+                })
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'event_count': event_count,
+    }
+
+    return context

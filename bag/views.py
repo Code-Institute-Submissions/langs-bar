abe291120a74e2ee6e61357f5fc901fd4cdfe019
TechCentreUK <support@techcentreuk.co.uk>
2022-02-12 22:58:08 +0000

    Add message toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from events.models import Event
 
 
 # Create your views here.
@@ -8,9 +10,11 @@ def view_bag(request):
     """ A View to return the bag.html """
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified event to the shopping bag """
 
+    event = Event.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -18,18 +22,21 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {event.name} in your bag.')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {event.name} to your bag.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified event to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
-    
+
     if quantity > 0:
         bag[item_id] = quantity
     else:

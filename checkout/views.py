8cd1e98a8c7847008d09eee2a3c5fbc802cdd43b
TechCentreUK <support@techcentreuk.co.uk>
2022-02-23 21:09:44 +0000

    Add checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-import stripe
-from bag.contexts import bag_contents
-from events.models import Event
+
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+from events.models import Event
+from bag.contexts import bag_contents
+
+import stripe
+import json
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
-    """ Checkout page """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -40,6 +59,7 @@ def checkout(request):
                             quantity=item_data,
                         )
                         order_line_item.save()
+
                 except Event.DoesNotExist:
                     messages.error(request, (
                         "One of the products in your bag wasn't found in our database. "
@@ -57,7 +77,7 @@ def checkout(request):
         bag = request.session.get('bag', {})
         if not bag:
             messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('products'))
+            return redirect(reverse('events'))
 
         current_bag = bag_contents(request)
         total = current_bag['total']

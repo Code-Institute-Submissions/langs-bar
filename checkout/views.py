7201b33343c55525c08712dd2cb2da84e9c62197
TechCentreUK <support@techcentreuk.co.uk>
2022-03-17 20:35:34 +0000

    Add ability to checkout free events

--- checkout/views.py
+++ checkout/views.py
@@ -30,6 +30,7 @@ def cache_checkout_data(request):
 
 
 def checkout(request):
+    """ A View to checkout products  """
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -51,9 +52,15 @@ def checkout(request):
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_bag = json.dumps(bag)
-            order.save()
+            current_bag = bag_contents(request)
+            total = current_bag['total']
+            if total == 0:
+                order.original_bag = json.dumps(bag)
+                order.save()
+            else:
+                order.stripe_pid = pid
+                order.original_bag = json.dumps(bag)
+                order.save()
             for item_id, item_data in bag.items():
                 try:
                     event = Event.objects.get(id=item_id)
@@ -86,12 +93,13 @@ def checkout(request):
 
         current_bag = bag_contents(request)
         total = current_bag['total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+        if total != 0:
+            stripe_total = round(total * 100)
+            stripe.api_key = stripe_secret_key
+            intent = stripe.PaymentIntent.create(
+                amount=stripe_total,
+                currency=settings.STRIPE_CURRENCY,
+            )
 
         # Attempt to prefill the form with any info
         # the user maintains in their profile
@@ -120,11 +128,16 @@ def checkout(request):
             Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+    if total == 0:
+        context = {
+            'order_form': order_form,
+        }
+    else:
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
 
     return render(request, template, context)
 

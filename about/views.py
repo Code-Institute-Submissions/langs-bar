f67ce40a3b7c18120e58f15340ebc8f90bcbd944
TechCentreUK <support@techcentreuk.co.uk>
2022-03-23 20:58:17 +0000

    Add Privacy Policy

--- about/views.py
+++ about/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 def about(request):
     """ A View to return the about.html """
     return render(request, 'about/about.html')
+
+def privacy(request):
+    """ A View to return the privacy.html """
+    return render(request, 'about/privacy.html')
